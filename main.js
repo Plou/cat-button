050193868806c4be413fb563b097897c56899b70
Plou <Plou@users.noreply.github.com>
2018-10-04 00:35:54 +0200

    fix: Update API compatibility

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
-var $catBox = document.querySelector('#cat-box')
+var $catImg = document.querySelector('#cat-img')
 var $refreshButton = document.querySelector('#refresh-cat')
-var $cats = ['space', 'hats', 'space', 'funny', 'sunglasses', 'boxes', 'caturday', 'ties', 'dream', 'kittens', 'sinks', 'clothes']
-var $cat = $cats[Math.floor(Math.random() * $cats.length)]
 
-$refreshButton.addEventListener('click', (e) => {
-  e.preventDefault()
-  fetch(`https://thecatapi.com/api/images/get?api_key=MzM1MDk2&format=html&category=${$cat}`)
-  .then((data) => data.text())
-  .then((img) => $catBox.innerHTML = img)
+var getCategories = function () {
+  return fetch('https://api.thecatapi.com/v1/categories?api_key=MzM1MDk2', {mode: "cors"})
+  .then((data) => data.json())
+}
+
+var getCat = function (category) {
+  return fetch(`https://api.thecatapi.com/v1/images/search?api_key=MzM1MDk2&category_ids=${category}`, {mode: "cors"})
+  .then((data) => data.json())
+  .then((data) => data[0].url )
+}
+
+getCategories()
+  .then((categories) => {
+    var category = categories[Math.floor(Math.random() * categories.length)]
+    console.log(category.name)
+    return category.id
+  })
+  .then((category) => {
+    getCat(category)
+      .then((url) => {
+        $catImg.setAttribute('src', url)
+      })
+    $refreshButton.addEventListener('click', (e) => {
+      e.preventDefault()
+      getCat(category)
+        .then((url) => {
+          $catImg.setAttribute('src', url)
+        })
+    })
   .catch(console.log)
 })
-
-fetch(`https://thecatapi.com/api/images/get?api_key=MzM1MDk2&format=html&category=${$cat}`)
-  .then((data) => data.text())
-  .then((img) => $catBox.innerHTML = img)
-  .catch(console.log)
